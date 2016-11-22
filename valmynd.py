b0381fc37b6e700edc6dfb419c3ba2bbedbd3b33
Natsumi2813 <kristjan@ragnars.net>
2016-11-22 14:58:40 +0000

    Valmynd er tilbuin fyrir beta test

--- valmynd.py
+++ valmynd.py
@@ -1,19 +1,21 @@
 # Valmynd gert af Kristjáni 22/11/2016 13:22
 
 # Imports
+from Lidur1 import *
+from Lidur2 import *
 from time import sleep
 
 while True:
     # valmyndin
-    print("1. Bílar \n2. Samlagning \n3. Skæri, blað, steinn \n4. Strengur \n5. Heiltölur \n 6.Teningaspilið Craps"
+    print("1. Bílar \n2. Samlagning \n3. Skæri, blað, steinn \n4. Strengur \n5. Heiltölur \n6.Teningaspilið Craps"
           "\n7. Teningakast \n8. byggingakast \n9. Hætta")
 
     x1 = input("Veldu valmögu leika: ")
 
     if x1 == 1:
-        print()
+        Lidur1.Main()
     elif x1 == 2:
-        print()
+        Lidur2.main()
     elif x1 == 3:
         print()
     elif x1 == 4:
