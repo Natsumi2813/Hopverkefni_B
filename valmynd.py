a2eb1ae71f4c75cc9d7234ab1ab385e8c7a62acf
Natsumi2813 <kristjan@ragnars.net>
2016-11-22 13:34:49 +0000

    Valmynd

---
+++ valmynd.py
@@ -0,0 +1,31 @@
+# Valmynd gert af Kristjáni 22/11/2016 13:22
+
+# Imports
+from time import sleep
+
+while True:
+
+    print("1. Bílar \n2. Samlagning \n3. Skæri, blað, steinn \n4. Strengur \n5. Heiltölur \n 6.Teningaspilið Craps"
+          "\n7. Teningakast \n8. byggingakast \n9. Hætta")
+
+    x1 = input("Veldu valmögu leika: ")
+
+    if x1 == 1:
+        print()
+    elif x1 == 2:
+        print()
+    elif x1 == 3:
+        print()
+    elif x1 == 4:
+        print()
+    elif x1 == 5:
+        print()
+    elif x1 == 6:
+        print()
+    elif x1 == 7:
+        print()
+    elif x1 == 8:
+        print()
+    elif x1 == 9:
+        print("Slekk á forriti")
+        sleep(1)
