ed7140cfcc0a996e7bfcf5e58234fc7837d00e7c
Gudmunduro <goh140400@gmail.com>
2016-11-23 10:22:23 +0000

    Daniel Hallo

---
+++ myClass.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+ namespace myNamespace
+ {
+ 	public class myClass
+ 	{
+ 		public static void Main(string[] args)
+ 		{
+ 			//Your code goes here
+ 			Console.WriteLine("Halló Daniel");
+ 		}
+ 	}
+ }
