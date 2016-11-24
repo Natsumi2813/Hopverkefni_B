13c73937d9d4853d43599eea4b1d8992fa234366
Natsumi2813 <kristjan@ragnars.net>
2016-11-24 10:58:47 +0000

    lidur 4 Komin

---
+++ Lidur4.py
@@ -0,0 +1,16 @@
+# gert af Kristjáni 24/11/2016
+
+class Lidur4:
+    def breiti_streng(self, x):
+        for i in ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "o", "p", "q", "r", "t", "u", "v", "w",
+                  "x", "y", "z", "á", "ó", "ö", "í", "ú", "ý", "é", "ð", "þ", "æ", "B", "C", "D", "E", "F", "G", "H",
+                  "I", "J", "K", "L", "M", "O", "Ó", "P", "Q", "R", "T", "U", "V", "W", "X", "Y", "Z", "Á", "Ö", "Í",
+                  "Ú", "Ý", "É", "Ð", "Þ", "Æ"]:
+            if i in x:
+                x = x.replace(i, '*')
+        return x
+    def main(self):
+        x = str(input("Slæaðu inn setningu: "))
+        print(self.breiti_streng(x))
+
+
